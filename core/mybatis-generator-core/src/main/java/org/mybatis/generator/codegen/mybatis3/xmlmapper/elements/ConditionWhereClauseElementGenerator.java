0fa5cb7e47e1a26951ebdcf42007f58cc15f87f8
root <yzhuomaoy@gmail.com>
2016-12-06 15:41:57 +0800

    init

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/ConditionWhereClauseElementGenerator.java
@@ -0,0 +1,79 @@
+/**
+ *    Copyright 2006-2016 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.codegen.mybatis3.xmlmapper.elements;
+
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
+
+/**
+ * 
+ * @author Jeff Butler
+ * 
+ */
+public class ConditionWhereClauseElementGenerator extends
+        AbstractXmlElementGenerator {
+
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("sql"); //$NON-NLS-1$
+
+        answer.addAttribute(new Attribute(
+                "id", introspectedTable.getConditionWhereClauseId())); //$NON-NLS-1$
+
+        context.getCommentGenerator().addComment(answer);
+
+        XmlElement whereElement = new XmlElement("where"); //$NON-NLS-1$
+        answer.addElement(whereElement);
+
+        StringBuilder sb = new StringBuilder();
+        for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+        	XmlElement insertNotNullElement = new XmlElement("if"); //$NON-NLS-1$
+            sb.setLength(0);
+            sb.append(introspectedColumn.getJavaProperty());
+            sb.append(" != null"); //$NON-NLS-1$
+            insertNotNullElement.addAttribute(new Attribute(
+                    "test", sb.toString())); //$NON-NLS-1$
+
+            
+            sb.setLength(0);
+            sb.append(" and ");
+            sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            sb.append('=');
+            sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+            insertNotNullElement.addElement(new TextElement(sb.toString()));
+            
+            whereElement.addElement(insertNotNullElement);
+        }
+
+
+
+//        if (context.getPlugins()
+//                .sqlMapExampleWhereClauseElementGenerated(answer,
+//                        introspectedTable)) {
+//            parentElement.addElement(answer);
+//        }
+        
+        parentElement.addElement(answer);
+    }
+
+}
---
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/MyBatisGeneratorTest2.java
@@ -0,0 +1,53 @@
+/**
+ *    Copyright 2006-2016 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.ConnectionFactoryConfiguration;
+import org.mybatis.generator.config.Context;
+import org.mybatis.generator.config.JDBCConnectionConfiguration;
+import org.mybatis.generator.config.ModelType;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MyBatisGeneratorTest2 {
+
+    @Test
+    public void testGenerateMyBatis3Config() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("generatorConfig.xml"));
+            
+        DefaultShellCallback shellCallback = new DefaultShellCallback(false);
+        
+        try {
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+            myBatisGenerator.generate(null, null, null, true);
+            System.out.println(myBatisGenerator.getGeneratedJavaFiles());
+            System.out.println(myBatisGenerator.getGeneratedXmlFiles());
+        } catch (InvalidConfigurationException e) {
+            throw e;
+        }
+    }
+}
